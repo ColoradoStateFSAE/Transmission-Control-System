f812238faab7fe7a6fb214301d2a9eb295a3aeac
Aidan Farley <45497901+Sandman224@users.noreply.github.com>
2024-05-20 17:47:52 -0600

    Update Transmission.cpp
    Added auto clutch for low RPM upshifts

--- src/Transmission/Transmission.cpp
+++ src/Transmission/Transmission.cpp
@@ -23,7 +23,12 @@ void Transmission::shift(Transmission::Direction direction) {
 
 	if(direction == UP) {
 		Serial.println("\nUP");
-		state(UP_SPARK_CUT);
+		if(can.rpm <= 6000 && clutch.input < 90) {
+			state(UP_CLUTCH_IN);
+		} else {
+			state(UP_SPARK_CUT);
+		}
+		
 	} else if(direction == DOWN) {
 		Serial.println("\nDOWN");
 		state(DOWN_CLUTCH_IN);
@@ -34,15 +39,33 @@ void Transmission::upRoutine() {
 	const int pin = storage.IA;
 
 	switch(state()) {
+
+		case UP_CLUTCH_IN: {
+			runOnce([&](){
+				Serial.println("CLUTCH IN " + String(millis() - shiftStartTime));
+				clutch.state(Clutch::State::IDLE);
+				clutch.writeMicroseconds(storage.end());
+			});
+
+			// Allow a shorter delay if the clutch paddle is already in
+			int delay = storage.upDelay();
+			if(clutch.input >= 90) {
+				delay = storage.upDelay();
+			}
+
+			// Wait for the delay and set next state
+			waitAndSetState(delay, [](){}, UP_ENABLE_SOLENOID);
+			break;
+		}
+
 		case UP_SPARK_CUT:
 			runOnce([&](){
 				Serial.println("COMBUSTION DISABLE: " + String(millis() - shiftStartTime));
 				digitalWrite(storage.ECU_UP, LOW);
 			});
 
-			waitAndSetState(storage.upDelay(), [](){
-				// Wait for the up delay
-			}, UP_ENABLE_SOLENOID);
+			// Wait for the up delay and set next state
+			waitAndSetState(storage.upDelay(), [](){}, UP_ENABLE_SOLENOID);
 			break;
 
 		case UP_ENABLE_SOLENOID:
@@ -52,21 +75,44 @@ void Transmission::upRoutine() {
 				digitalWrite(13, HIGH);
 			});
 
-			waitAndSetState(storage.output(), [](){
-				// Wait for the output duration
-			}, UP_DISABLE_SOLENOID);
+			// Wait for the output duration and set next state
+			waitAndSetState(storage.output(), [](){}, UP_DISABLE_SOLENOID);
 			break;
 
-		case UP_DISABLE_SOLENOID:
+		case UP_DISABLE_SOLENOID: {
 			runOnce([&](){
 				Serial.println("SOLENOID DISABLE: " + String(millis() - shiftStartTime));
 				digitalWrite(pin, LOW);
-				digitalWrite(storage.ECU_UP, HIGH);
 				digitalWrite(13, LOW);
+			});
+
+			// Default parameters
+			int delay = 50;
+			State nextState = UP_CLUTCH_OUT;
+			
+			// Switch directly to IDLE if the clutch paddle is already in or if clutch is already out
+			if(clutch.input >= 90 || clutch.position() == storage.start()) {
+				delay = 0;
+				nextState = IDLE;
+				clutch.state(Clutch::State::ANALOG_INPUT);
+			}
+
+			// Wait for set delay and set next state
+			waitAndSetState(delay, [](){}, nextState);
+			break;
+		}
+
+		case UP_CLUTCH_OUT: {
+			runOnce([&](){
+				Serial.println("CLUTCH OUT " + String(millis() - shiftStartTime));
+				clutch.writeMicroseconds(storage.start());
+				clutch.state(Clutch::State::ANALOG_INPUT);
 			});
 
 			state(IDLE);
 			break;
+		}
+
 	}
 }
 
@@ -81,15 +127,14 @@ void Transmission::downRoutine() {
 				clutch.writeMicroseconds(storage.end());
 			});
 
-			// Allow a shorter delay if the clutch is already in
+			// Allow a shorter delay if the clutch paddle is already in
 			int delay = storage.downDelay();
 			if(clutch.input >= 90) {
 				delay = storage.upDelay();
 			}
 
-			waitAndSetState(delay, [](){
-				// Wait for the delay
-			}, DOWN_ENABLE_SOLENOID);
+			// Wait for the delay and set next state
+			waitAndSetState(delay, [](){}, DOWN_ENABLE_SOLENOID);
 			break;
 		}
 
@@ -100,9 +145,8 @@ void Transmission::downRoutine() {
 				digitalWrite(13, HIGH);
 			});
 
-			waitAndSetState(storage.output(), [](){
-				// Wait output duration
-			}, DOWN_DISABLE_SOLENOID);
+			// Wait output duration and set next state
+			waitAndSetState(storage.output(), [](){}, DOWN_DISABLE_SOLENOID);
 			break;
 		}
 
@@ -113,7 +157,7 @@ void Transmission::downRoutine() {
 				digitalWrite(13, LOW);
 			});
 
-			// Switch directly to IDLE if the clutch is already in
+			// Switch directly to IDLE if the clutch paddle is already in
 			int delay = 50;
 			State nextState = DOWN_CLUTCH_OUT;
 			if(clutch.input >= 90) {
@@ -121,10 +165,9 @@ void Transmission::downRoutine() {
 				nextState = IDLE;
 				clutch.state(Clutch::State::ANALOG_INPUT);
 			}
-
-			waitAndSetState(delay, [](){
-
-			}, nextState);
+			
+			// Wait for set delay and set next state
+			waitAndSetState(delay, [](){}, nextState);
 			break;
 		}
 
