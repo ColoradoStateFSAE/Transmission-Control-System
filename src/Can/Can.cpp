626ac7d91534a9b1cde5d4a2a879f9dc1d91a8d2
Aidan Farley <45497901+Sandman224@users.noreply.github.com>
2024-09-21 13:27:25 -0600

    Update Can.cpp

--- src/Can/Can.cpp
+++ src/Can/Can.cpp
@@ -21,8 +21,7 @@ void Can::update() {
 				lastCanUpdate = millis();
 				r3_group0_t message;
 				r3_group0_unpack(&message, msg.buf, sizeof(msg.buf));
-				_rpm = r3_group0_rpm_decode(message.rpm);
-				clutch.rpm = r3_group0_rpm_decode(message.rpm);
+				rpm = r3_group0_rpm_decode(message.rpm);
 				break;
 			}
 
@@ -60,9 +59,10 @@ void Can::update() {
 	}
 
 	if(millis() - lastCanUpdate >= 100) {
-		_rpm = 0;
+		rpm = 0;
 	}
 }
+
 
 void Can::broadcastClutchPosition(int value, unsigned long frequency) {
 	static int lastBroadastTime = 0;
